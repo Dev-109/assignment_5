80bff2ff3e226cd5eb34df506a6d29b388e08205
Dev-109 <55503392+Dev-109@users.noreply.github.com>
2022-03-17 14:06:33 -0400

    Update assignment_5.js

--- assignment_5.js
+++ assignment_5.js
@@ -1,8 +1,48 @@
 $(document).ready(function(){
-
+var flag = 0;
     $("#row_header1").click(function(event){
+    
+        if(flag==0){
         d3.select("#row1").style("background-color", "#e0e0ff");
+        flag =1;
+        }
+        else{
+            d3.select("#row1").style("background-color", "white");
+            flag = 0;
+        }
+        
 
       });
+
+
+      $("#row_header2").click(function(event){
+    
+        if(flag==0){
+        d3.select("#row2").style("background-color", "#e0e0ff");
+        flag =1;
+        }
+        else{
+            d3.select("#row2").style("background-color", "white");
+            flag = 0;
+        }
+        
+
+      });
+
+      $("#row_header3").click(function(event){
+    
+        if(flag==0){
+        d3.select("#row3").style("background-color", "#e0e0ff");
+        flag =1;
+        }
+        else{
+            d3.select("#row3").style("background-color", "white");
+            flag = 0;
+        }
+        
+
+      });
+
+
 
 });
